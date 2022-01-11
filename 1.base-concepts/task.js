3ab8f5bc917646b20d6512c03ae96c168372ff77
Korotkov Dmitry <2110821@mail.ru>
2022-01-11 21:37:45 +0300

    JS1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,61 @@
+"console strict"
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  // ax^2+bx+c = 0
+  let arr = [];
+  let D = b ** 2 - 4 * a * c;
+  if (D < 0) {
+    arr = [];
+    return arr;
+  } else if (D == 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
+    return arr;
+  } else if (D > 0) {
+    let x1 = (-b + Math.sqrt(D)) / (2 * a);
+    let x2 = (-b - Math.sqrt(D)) / (2 * a);
+    arr.push(x1);
+    arr.push(x2);
+    return arr;
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+  let nowDate = new Date();
+  let percentN = Number(percent);
+  let contributionN = Number(contribution);
+  let amountN = Number(amount);
+  if (Number.isNaN(percentN)) {
+    let error = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+    return error;
+  };
+  if (Number.isNaN(contributionN)) {
+    let error = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+    return error;
+  };
+  if (Number.isNaN(amountN)) {
+    let error = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+    return error;
+  };
+  let fractionalPercent = Number(percentN) / 100;
+  let creditBody = amountN - contributionN; // тело кредита
+  let numberOfMonthes = monthDiff(nowDate, date); // количество месяцев
+  let oneTwelfthOfPercent = fractionalPercent / 12;  // 1/12 процентной ставки
+  let monthlyLoanPayment = creditBody * (oneTwelfthOfPercent + (oneTwelfthOfPercent / (((1 + oneTwelfthOfPercent) ** numberOfMonthes) - 1)));  // ежемесячная плата
+  let totalAmountFull = contributionN + monthlyLoanPayment * numberOfMonthes;
   let totalAmount;
-
-  // код для задачи №2 писать здесь
-
+  if (contributionN === amountN) {
+    totalAmount = 0
+  } else {
+    totalAmount = Number(totalAmountFull.toFixed(2));
+  }
+  console.log(totalAmount);
   return totalAmount;
+}
+
+function monthDiff(d1, d2) {
+  var months;
+  months = (d2.getFullYear() - d1.getFullYear()) * 12;
+  months -= d1.getMonth();
+  months += d2.getMonth();
+  return months <= 0 ? 0 : months;
 }
