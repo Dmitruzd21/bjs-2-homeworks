987ac2e381e9fe5be875d143da908f38dbd9f164
Korotkov Dmitry <2110821@mail.ru>
2022-01-12 18:43:06 +0300

    JS2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,45 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    };
+    if (arr[i] > max) {
+      max = arr[i];
+    };
+    sum += arr[i];
+  }
+  let avg = sum / arr.length;
+  return { min: min, max: max, avg: Number(avg.toFixed(2)) };
 }
 
+
 // Задание 2
-function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+function worker(arr) {                 // насадка 1
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
   return sum;
 }
 
-function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+function makeWork(arrOfArr, func) {   // мясорубка
+  let max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let sum = func(arrOfArr[i]);
+    if (sum > max) {
+      max = sum;
+    }
+  }
   return max;
 }
 
-// Задание 3
-function worker2(arr) {
-  // Ваш код
+// Задание 3  
+function worker2(arr) {              // насадка 2
+  let arrayParams = getArrayParams(arr);
+  let diff = Math.abs(arrayParams.max - arrayParams.min);
+  return diff;
 }
