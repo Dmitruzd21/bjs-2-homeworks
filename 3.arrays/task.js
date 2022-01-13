d2ea5a9ee5e00af9a9dd3ca1ac0812c7c7ad9bae
Korotkov Dmitry <2110821@mail.ru>
2022-01-13 19:55:06 +0300

    JS3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,17 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
-  return result; // boolean
+  if (arr1.length > arr2.length || arr1.length < arr2.length) {
+    return false;
+  } else {
+    result = arr1.every ((currentValuefromArr1, index) => currentValuefromArr1 === arr2[index]);
+    return result;
+  }
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let resultArr= arr
+  .filter((item) => item > 0)
+  .filter ((item) => item % 3 === 0)
+  .map ((item) => item * 10);
+  return resultArr;
 }
