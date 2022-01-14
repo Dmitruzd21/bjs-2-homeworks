3966167b4f5baa08274109dc17c8489117fdb224
Korotkov Dmitry <2110821@mail.ru>
2022-01-14 20:49:54 +0300

    JS4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,51 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+let student1 = new Student("Oleg", "male", 37);
+let student2 = new Student("Ann", "female", 29);
+let student3 = new Student("Victor", "male", 21);
+
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  if (this.marks === undefined) {
+    this.marks = new Array();
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function(...marks) {
+  if (this.marks === undefined) {
+    this.marks = new Array();
+    for (i = 0; i < marks.length; i++) {
+      this.marks.push(marks[i]);
+    }
+  } else {
+    for (i = 0; i < marks.length; i++) {
+      this.marks.push(marks[i]);
+    }
+  }
+}
+
+Student.prototype.getAverage = function() {
+  let allMarks = this.marks;
+  let sum = 0;
+  for (i = 0; i < allMarks.length; i++) {
+    sum += allMarks[i];
+  }
+  let averageOfmarks = sum / allMarks.length;
+  return averageOfmarks;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
