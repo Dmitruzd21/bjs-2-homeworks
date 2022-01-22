4e9456cd081350248f0f28de31a72e314ecaba21
Korotkov Dmitry <2110821@mail.ru>
2022-01-22 22:02:22 +0300

    JS8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,10 +1,11 @@
+// Задача 1. Усовершенствуйте кэширующий декоратор
+
 function cachingDecoratorNew(func) {
   let cache = [];
   return function wrapper(...rest) {
     const key = rest.join(",");
     let index = cache.findIndex((element) => element.key === key);
     if (index !== -1) {   // если элемент найден
-
       console.log("Из кэша: " + cache[index].value);
       return "Из кэша: " + cache[index].value;
     }
@@ -18,10 +19,46 @@ function cachingDecoratorNew(func) {
   }
 }
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+// Задача 2. Debounce декоратор с моментальным вызовом
+
+function debounceDecoratorNew(func, ms) {
+  let timeout;
+  let flag = true;
+  return function wrapper(...rest) {
+    //Взведён ли флаг? Можно ли выполнить функцию синхронно?
+    if (flag) {
+      func(...rest);
+      // Синхронный вызов произошёл - взводим флаг, то есть говорим, что пока синхронные вызовы не нужны 
+      flag = false;
+    }
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func(...rest);
+     // Синхронный вызов произошёл - взводим флаг, то есть говорим, что пока синхронные вызовы не нужны 
+      flag = true;
+    }, ms);
+  }
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+// Задача 3. Усовершенствуйте debounceDecoratorNew
+
+function debounceDecorator2(func, ms) {
+  let timeout;
+  let flag = true;
+  function wrapper(...rest) {
+    wrapper.count += 1;
+    if (flag) {
+      func(...rest);
+      flag = false;
+      return;
+    }
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func(...rest);
+      flag = true;
+    }, ms);
+  }
+  wrapper.count = 0;
+  wrapper.getCount = () => console.log("Количество вызовов: " + wrapper.count);
+  return wrapper;
 }
